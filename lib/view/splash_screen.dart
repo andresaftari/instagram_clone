df26fd57bf85f18880803142ebb5ba94fd527cc6
andresaftari <andresaftari@gmail.com>
2021-12-15 10:11:31 +0700

    Fixed gradle problem

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({Key? key}) : super(key: key);
-
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
